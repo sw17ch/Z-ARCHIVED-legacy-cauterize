22f76d67d586a2618b450a67f605656befaa084c
John Van Enk <vanenkj@gmail.com>
2012-11-28 14:14:53 -0500

    Always use a length field.

--- rb/lib/group.rb
+++ rb/lib/group.rb
@@ -36,7 +36,7 @@ class Group
     uses_length = @members.any? {|m| m.sizeFunc}
     formatter.struct(@name) do |f|
       f << "#{enum_type} tag;"
-      f << "size_t length;" if uses_length
+      f << "size_t length;"
       f.braces("union") do |g|
         @members.each {|m| g << "struct #{m.name} #{m.name.down_snake};"}
       end
@@ -52,6 +52,7 @@ class Group
       f.blank_line
 
       # Copy the tag.
+      f << "/* Copy the tag. */"
       f << "s = CauterizeAppend(dst, &(src->tag), sizeof(src->tag));"
       f.braces("if (CA_OK != s)") do
         f << "return s;"
@@ -61,10 +62,22 @@ class Group
       # Copy the union.
       f.braces("switch (src->tag)") do
         @members.each do |m|
-          f.undented { f << "case #{m.enum_name(name_as_prefix)}:"}
+          # Create the field
           field = "src->data.#{m.name.down_snake}"
+          f.undented { f << "case #{m.enum_name(name_as_prefix)}:"}
+
+          # Update and copy the length
+          f << "/* Copy the length. */"
           sizeFunc = m.sizeFunc || "sizeof"
-          f << "s = CauterizeAppend(dst, &(#{field}), #{sizeFunc}(#{field}));"
+          f << "src->length = #{sizeFunc}(#{field})"
+          f << "s = CauterizeAppend(dst, &(src->length), sizeof(src->length));"
+          f.braces("if (CA_OK != s)") do
+            f << "return s;"
+          end
+          f.blank_line
+
+          # Copy the data
+          f << "s = CauterizeAppend(dst, &(#{field}), src->length);"
           f.braces("if (CA_OK != s)") do
             f << "return s;"
           end
@@ -72,6 +85,7 @@ class Group
           f.blank_line
         end
       end
+      f.blank_line
       f << "return CA_OK;"
     end
     formatter.blank_line
@@ -81,6 +95,7 @@ class Group
     params = [ "struct #{@name} * dst", "struct Cauterize * src" ]
     formatter.func("Unpack#{@name}", "CAUTERIZE_STATUS_T", params) do |f|
       f << "CAUTERIZE_STATUS_T s;"
+      f << "size_t length = 0;"
       f.blank_line
 
       # Read the tag.
@@ -90,13 +105,20 @@ class Group
       end
       f.blank_line
 
+      # Copy the length
+      f << "s = CauterizeRead(src, &(dst->length), sizeof(dst->length));"
+      f.braces("if (CA_OK != s)") do
+        f << "return s;"
+      end
+      f.blank_line
+
       # Read the union based on the tag.
       f.braces("switch (dst->tag)") do
         @members.each do |m|
-          f.undented { f << "case #{m.enum_name(name_as_prefix)}:"}
           field = "dst->data.#{m.name.down_snake}"
-          sizeFunc = m.sizeFunc || "sizeof"
-          f << "s = CauterizeRead(src, &(#{field}), #{sizeFunc}(#{field}));"
+          f.undented { f << "case #{m.enum_name(name_as_prefix)}:"}
+
+          f << "s = CauterizeRead(src, &(#{field}), &(dst->length));"
           f.braces("if (CA_OK != s)") do
             f << "return s;"
           end
@@ -104,6 +126,7 @@ class Group
           f.blank_line
         end
       end
+      f.blank_line
       f << "return CA_OK;"
     end
     formatter.blank_line
