2b2e415f9c8f9580312f720c4b443ca73c57f1d6
John Van Enk <vanenkj@gmail.com>
2012-11-28 12:17:00 -0500

    Move String extensions elsewhere.

--- rb/group.rb
+++ rb/group.rb
@@ -1,19 +1,4 @@
-class String
-  def snake
-    self.gsub(/::/, '/').
-    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-    gsub(/([a-z\d])([A-Z])/,'\1_\2').
-    tr("-", "_")
-  end
-
-  def up_snake
-    snake.upcase
-  end
-
-  def down_snake
-    snake.downcase
-  end
-end
+require 'mixins'
 
 class Member
   attr_reader :name, :sizeFunc
