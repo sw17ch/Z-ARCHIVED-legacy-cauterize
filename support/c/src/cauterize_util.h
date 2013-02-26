18c2d9071d4574545427f1e36addaa1757ed17fd
John Van Enk <vanenkj@gmail.com>
2013-02-26 12:03:52 -0500

    Rejigger things a bit so that we only define CALLCONV when building as a DLL.

--- support/c/src/cauterize_util.h
+++ support/c/src/cauterize_util.h
@@ -6,17 +6,25 @@
 
 /* Buidling Cauterize files as a DLL. */
 
+#if defined(BUILDING_DLL) || defined(USING_DLL)
+  #if !defined(_WIN32)
+    #error "Can only build DLL in Windows."
+  #endif
+
+  #define CALLCONV __stdcall
+#endif
+
 #if defined(BUILDING_DLL)
-#define DLLDECL __declspec(dllexport)
+  #define DLLDECL __declspec(dllexport)
 #elif defined(USING_DLL)
-#define DLLDECL __declspec(dllimport)
-#else
+  #define DLLDECL __declspec(dllimport)
+#endif
+
+#ifndef DLLDECL
 #define DLLDECL
 #endif
 
-#if defined(_WIN32)
-#define CALLCONV __stdcall
-#else
+#ifndef CALLCONV
 #define CALLCONV
 #endif
 
