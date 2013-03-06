1f49abf0191d3110a76efb4b0c0affbfccdc3975
John Van Enk <vanenkj@gmail.com>
2013-03-05 23:53:31 -0500

    Automatically scale enumeration representation based on max/min values.

--- lib/cauterize/enumeration.rb
+++ lib/cauterize/enumeration.rb
@@ -34,14 +34,13 @@ module Cauterize
   end
 
   class Enumeration < BaseType
-    attr_reader :values, :representation
+    attr_reader :values
 
     def initialize(name)
       super
       @values = {}
       @value_id = 0
       @used_ids = Set.new
-      @representation = BaseType.find_type!(:uint32)
     end
 
     def value(name, id=nil)
@@ -52,6 +51,23 @@ module Cauterize
       next_id = value_id(id)
       @used_ids << next_id
       @values[name] = EnumerationValue.new(name, next_id)
+    end
+
+    def representation
+      max_val = @values.values.map(&:value).max
+      min_val = @values.values.map(&:value).min
+      
+      if -128 <= min_val and max_val <= 127
+        BaseType.find_type!(:int8)
+      elsif (-32768 <= min_val and max_val <= 32767)
+        BaseType.find_type!(:int16)
+      elsif (-2147483648 <= min_val and max_val <= 2147483647)
+        BaseType.find_type!(:int32)
+      elsif (-9223372036854775808 <= min_val and max_val <= 9223372036854775807)
+        BaseType.find_type!(:int64)
+      else
+        raise Exception.new("Unable to represent enumeration (#{min_val} -> #{max_val})")
+      end
     end
 
     private
